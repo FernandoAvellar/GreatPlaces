4cf35c4a9260b3504f534fd0fe356ab798233c68
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-10-18 11:41:15 -0300

    adicionado o cadastro de um novo lugar

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,18 +1,32 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:great_places/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
   final List<Place> _items = [];
 
-  List<Place> get getItems {
+  List<Place> get items {
     return [..._items];
   }
 
-  int get getItemsCount {
+  int get itemsCount {
     return _items.length;
   }
 
-  Place getItemByIndex(int index) {
+  Place itemByIndex(int index) {
     return _items[index];
+  }
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: Random().nextDouble().toString(),
+      title: title,
+      image: image,
+      location: '',
+    );
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,13 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  final Function onSelectImage;
+
+  const ImageInput({
+    super.key,
+    required this.onSelectImage,
+  });
 
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    String fileName = path.basename(_storedImage!.path);
+    final savedImage = await _storedImage!.copy(
+      '${appDir.path}/$fileName',
+    );
+    widget.onSelectImage(savedImage);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,14 +50,20 @@ class _ImageInputState extends State<ImageInput> {
           decoration: BoxDecoration(
             border: Border.all(width: 1, color: Colors.grey),
           ),
-          child: const Text('Nenhuma imagem!'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Nenhuma imagem!'),
         ),
         const SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
             icon: const Icon(Icons.camera),
             label: const Text('Tirar Foto'),
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
